7e39a5e705bc037d1f5a385c9b161ebf8842408e
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-01-18 19:42:43 -0500

    Add cdf and relu activation function

--- tagi/act.py
+++ tagi/act.py
@@ -1,4 +1,7 @@
 import math
+from scipy import stats
+from scipy.stats import norm
+import numpy as np
 
 """ Assign ID to activation functions
 This function assigns an ID number depending on the type of activation function.
@@ -12,7 +15,6 @@ This function assigns an ID number depending on the type of activation function.
 @return - 5 if \code{funName} is "softplus"
 @export """
 
-# 14
 def activationFunIndex(funName):
     if funName == "tanh":
         funIdx = 1
@@ -36,22 +38,34 @@ This function uses lineratization to estimate the activation units mean vector
 matrix evaluated at \eqn{\mu_{Z}}
 @export"""
 
-# 40
 def meanA(z, mz, funIdx):
     if funIdx == 1:
-
+        # tanh
         def dtanhf(x):
             print(1 - math.tanh(x) ^ 2)
-
         s = dtanhf(mz) * (z - mz) + math.tanh(mz)
         J = dtanhf(z)
     elif funIdx == 2:
-
+        # sigmoid
         def sigmoid(x):
             print(1 / (1 + math.exp(-x)))
-
         def dsigmoid(x):
             print(sigmoid(x) * (1 - sigmoid(x)))
-
         s = sigmoid(mz)
         J = dsigmoid(z)
+    elif funIdx == 3:
+        # cdf
+        #' In r this "stats::pnorm(0)" results "0.5" and in Python  "norm.cdf(0)" results "0.5"
+        #' Therefor norm.cdf in Python is equivalent with pnorm function in R
+        #' In r this "stats::dnorm(0)" results "0.3989" and in Python  "norm.pdf(0)" results "0.3989"
+        #' Therefor norm.pdf in Python is equivalent with dnorm function in R
+        s = norm.pdf(mz) * (z - mz) + norm.cdf(mz)
+        J = norm.pdf(z)
+    elif funIdx == 4:
+        #relu
+        #' numpys maximum() function in Python Equivalent pmax() in R
+        s = np.maximum(mz,0)
+
+    
+
+
