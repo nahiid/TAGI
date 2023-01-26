1f5df5d7a7e04ad6e3be473ef812ad4ba68aa2e8
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-01-25 23:56:30 -0500

    Define split and normalize function

--- tagi/dp.py
+++ tagi/dp.py
@@ -8,8 +8,33 @@ This function denormalizes response data processed by the neural network.
 @return - Mean of denormalized predicted responses
 @return - Variance of denormalized predicted responses
 @export"""
+
+def normalize(xtrain, ytrain, xtest, ytest):
+    mxtrain = xtrain.mean(axis=0)
+    sxtrain = xtrain.std(axis=0)
+    mytrain = ytrain.mean(axis=0)
+    sytrain = ytrain.std(axis=0)
+    xntrain = (xtrain - mxtrain) / sxtrain
+    yntrain = (ytrain - mytrain) / sytrain
+    xntest = (xtest - mxtrain) / sxtrain
+    yntest = ytest
+    return xntrain, yntrain, xntest, yntest, mxtrain, sxtrain, mytrain, sytrain
+
+def split(x, y, ratio):
+    numObs = x.shape[0]
+    idxobs = np.random.permutation(numObs)
+    idxTrainEnd = int(ratio * numObs)
+    idxTrain = idxobs[:idxTrainEnd]
+    idxTest = idxobs[idxTrainEnd:]
+    xtrain = x[idxTrain,:]
+    ytrain = y[idxTrain,:]
+    xtest = x[idxTest,:]
+    ytest = y[idxTest,:]
+    return xtrain, ytrain, xtest, ytest
+
 def denormalize(yn, syn, myntrain, syntrain):
     y = yn * syntrain + myntrain
+    #. unsure
     if syn!=None:
         sy = syntrain^2 * syn
     else:
