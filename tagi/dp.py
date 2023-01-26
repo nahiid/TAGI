8a3e97a4da7713b812156c1bde377d3eda33fda3
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-01-26 00:05:30 -0500

    Complete the dp file

--- tagi/dp.py
+++ tagi/dp.py
@@ -1,4 +1,6 @@
+#Completed
 import numpy as np
+
 """Denormalize data
 This function denormalizes response data processed by the neural network.
 @param yn Predicted responses
@@ -8,7 +10,6 @@ This function denormalizes response data processed by the neural network.
 @return - Mean of denormalized predicted responses
 @return - Variance of denormalized predicted responses
 @export"""
-
 def normalize(xtrain, ytrain, xtest, ytest):
     mxtrain = xtrain.mean(axis=0)
     sxtrain = xtrain.std(axis=0)
@@ -18,8 +19,19 @@ def normalize(xtrain, ytrain, xtest, ytest):
     yntrain = (ytrain - mytrain) / sytrain
     xntest = (xtest - mxtrain) / sxtrain
     yntest = ytest
-    return xntrain, yntrain, xntest, yntest, mxtrain, sxtrain, mytrain, sytrain
+    outputs = np.array(xntrain, yntrain, xntest, yntest, mxtrain, sxtrain, mytrain, sytrain)
+    return outputs
 
+"""Split data
+This function splits data into training and test sets.
+@param x Input data
+@param y Response data
+@param ratio Training ratio
+@return - Training set of input variables
+@return - Training set of responses
+@return - Testing set of input variables
+@return - Testing set of responses
+@export"""
 def split(x, y, ratio):
     numObs = x.shape[0]
     idxobs = np.random.permutation(numObs)
@@ -30,14 +42,24 @@ def split(x, y, ratio):
     ytrain = y[idxTrain,:]
     xtest = x[idxTest,:]
     ytest = y[idxTest,:]
-    return xtrain, ytrain, xtest, ytest
+    outputs = np.array(xtrain, ytrain, xtest, ytest)
+    return outputs
 
+"""Denormalize data
+This function denormalizes response data processed by the neural network.
+@param yn Predicted responses
+@param syn Variance of the predicted responses
+@param myntrain Mean vector of responses from training set
+@param syntrain Variance vector of responses from training set
+@return - Mean of denormalized predicted responses
+@return - Variance of denormalized predicted responses
+@export"""
 def denormalize(yn, syn, myntrain, syntrain):
     y = yn * syntrain + myntrain
-    #. unsure
-    if syn!=None:
+    if syn is not None:
         sy = syntrain^2 * syn
     else:
         sy = None
     outputs = np.array(y, sy)
     return outputs
+
